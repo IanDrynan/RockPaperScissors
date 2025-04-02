90761a334bb6a4291ab66b7fa11eb4eb1783f838
Ian <drynanian@gmail.com>
2025-04-01 18:21:35 -0700

    Add UI elements

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+const choices = document.querySelector("#choices");
+choices.addEventListener("click", (event) => {
+  playRound(event.target.id);
+});
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", resetGame);
+
+function resetGame() {
+  document.getElementById("playerScore").textContent = 0;
+  document.getElementById("computerScore").textContent = 0;
+  document.getElementById("result").textContent = "";
+  reset.style.display = "none";
+}
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
@@ -9,6 +27,58 @@ function getComputerChoice() {
       return "scissors";
   }
 }
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
+  if (humanChoice == computerChoice) {
+    updateScores([0, 0]);
+  }
+  switch (humanChoice) {
+    case "rock":
+      switch (computerChoice) {
+        case "paper":
+          updateScores([0, 1]);
+          break;
+        case "scissors":
+          updateScores([1, 0]);
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerChoice) {
+        case "scissors":
+          updateScores([0, 1]);
+          break;
+        case "rock":
+          updateScores([1, 0]);
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerChoice) {
+        case "rock":
+          updateScores([0, 1]);
+          break;
+        case "paper":
+          updateScores([1, 0]);
+          break;
+      }
+      break;
+  }
+}
+function updateScores(scores) {
+  const playerScore = document.getElementById("playerScore");
+  const computerScore = document.getElementById("computerScore");
+  let newPlayerScore = parseInt(playerScore.textContent) + scores[0];
+  let newComputerScore = parseInt(computerScore.textContent) + scores[1];
+  if (newPlayerScore == 5 || newComputerScore == 5) {
+    document.getElementById("result").textContent =
+      newPlayerScore == 5 ? "You win!" : "You lose!";
+    reset.style.display = "flex";
+  }
+  playerScore.textContent = parseInt(playerScore.textContent) + scores[0];
+  computerScore.textContent = parseInt(computerScore.textContent) + scores[1];
+}
+//getHumanChoice() is deprecated
 function getHumanChoice() {
   const choices = ["rock", "paper", "scissors"];
   let choice = prompt("rock, paper, or scissors?");
@@ -17,44 +87,7 @@ function getHumanChoice() {
   }
   return choice;
 }
-
-function playRound(humanChoice, computerChoice) {
-  console.log(humanChoice, computerChoice);
-  if (humanChoice == computerChoice) {
-    console.log("tie");
-    return [0, 0];
-  }
-
-  switch (humanChoice) {
-    case "rock":
-      switch (computerChoice) {
-        case "paper":
-          console.log("You lose");
-          return [0, 1];
-        case "scissors":
-          console.log("You win");
-          return [1, 0];
-      }
-    case "paper":
-      switch (computerChoice) {
-        case "scissors":
-          console.log("You lose");
-          return [0, 1];
-        case "rock":
-          console.log("You win");
-          return [1, 0];
-      }
-    case "scissors":
-      switch (computerChoice) {
-        case "rock":
-          console.log("You lose");
-          return [0, 1];
-        case "paper":
-          console.log("You win");
-          return [1, 0];
-      }
-  }
-}
+//depcrecated
 function playGame(n) {
   let scores = [0, 0];
 
@@ -67,12 +100,10 @@ function playGame(n) {
   }
   if (scores[0] > scores[1]) {
     console.log("You win the game!");
-  }
-  else if (scores[0] == scores[1]) {
+  } else if (scores[0] == scores[1]) {
     console.log("You tie the game!");
   } else {
     console.log("You lose the game!");
   }
   console.log(scores);
 }
-playGame(5);
